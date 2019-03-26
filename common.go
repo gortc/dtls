a1f0846a481bcd9973dcb0e3f549221570b91314
Aleksandr Razumov <ar@gortc.io>
2019-03-26 03:30:12 +0300

    all: add DTLS version

--- common.go
+++ common.go
@@ -24,11 +24,12 @@ import (
 )
 
 const (
-	VersionSSL30 = 0x0300
-	VersionTLS10 = 0x0301
-	VersionTLS11 = 0x0302
-	VersionTLS12 = 0x0303
-	VersionTLS13 = 0x0304
+	VersionSSL30  = 0x0300
+	VersionTLS10  = 0x0301
+	VersionTLS11  = 0x0302
+	VersionTLS12  = 0x0303
+	VersionTLS13  = 0x0304
+	VersionDTLS12 = 0xfefd
 )
 
 const (
